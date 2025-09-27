9bf4d5aa873a560a885e905bfc821b2c81536061
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2025-09-27 10:09:53 -0400

    feat: entry point of server

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const express = requires("express");
-const mongoose = requires("mongoose");
-const cors = requires("cors");
+// entry point file
 
+const express = requires("express");
+const cors = requires("cors");
 require('dotenv').config();
+
+const db = require("./db"); // initializes mongodb
+const raceRoutes = require("./routes/raceRoutes");
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+app.use("/races", raceRoutes)
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log("Listening on port ${PORT}"));
